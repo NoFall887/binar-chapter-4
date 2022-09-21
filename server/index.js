f006e4e24f5490dec9128d9cc225617b642058b3
NoFall887 <naufalamiruddin123456@gmail.com>
2022-09-21 20:17:29 +0700

    httpserver

--- server/index.js
+++ server/index.js
@@ -1,1 +1,47 @@
-console.log("Implement servermu disini yak 😝!");
+const http = require("http");
+const { PORT = 8000 } = process.env; // Ambil port dari environment variable
+
+const fs = require("fs");
+const path = require("path");
+const public = path.join(__dirname, "../public");
+
+function getFile(fileName) {
+  const filePath = path.join(public, fileName);
+  return fs.readFileSync(filePath);
+}
+
+function onRequest(req, res) {
+  switch (req.url) {
+    case "/":
+      res.writeHead(200);
+      res.end(getFile("index.html"));
+      break;
+    case "/cars":
+      res.writeHead(200);
+      res.end(getFile("search.html"));
+      break;
+    default:
+      let file = path.join(public, decodeURI(req.url));
+      fs.readFile(file, function (err, data) {
+        if (err) {
+          console.log(err);
+          res.writeHead(404);
+          res.end(JSON.stringify(err));
+          return;
+        }
+        if (path.extname(file) === ".svg") {
+          res.setHeader("content-type", "image/svg+xml");
+        }
+        res.writeHead(200);
+        res.end(data);
+      });
+      break;
+  }
+}
+
+const server = http.createServer(onRequest);
+
+// Jalankan server
+server.listen(PORT, () => {
+  console.log("Server sudah berjalan, silahkan buka localhost:%d", PORT);
+});
